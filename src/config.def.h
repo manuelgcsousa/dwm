a2f82841665fe1bda69d57a2671b536cbe5fe93f
manuelgcsousa <manuel.sousa@tonicapp.com>
2024-01-02 12:25:44 +0000

    Change terminal to alacritty.

--- src/config.def.h
+++ src/config.def.h
@@ -89,7 +89,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/home/manuelgcsousa/Downloads/wezterm/usr/bin/wezterm", NULL };
+static const char *termcmd[]  = { "/home/manuelgcsousa/.cargo/bin/alacritty", NULL };
 
 static const Key keys[] = {
     /* modifier                     key                        function         argument */
